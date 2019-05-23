0259e7603dbc9e2ca03e5f77293d748f89e81f13
Corentin Molitor <corentin.molitor@gmail.com>
2019-05-23 13:16:45 +0100

    rewrote usage, -g is now optional, -o and -b have default values, added verbose option, changes to make compatible with python 3

--- plotReadLengths.py
+++ plotReadLengths.py
@@ -8,29 +8,53 @@ from subprocess import call, Popen, PIPE
 import os
 
 # Variable initialisation
-outputDir = None
-fastq = None
+outputDir = './'
+fasta = None
 lengthsFile = None
 expectedGenomeSize = None
 percent = None
+verbose = 0
 
+#===================================
+#		Functions
+#===================================
 def usage():
-	print("-h for help\n\nInput:\n\t-g for estimated genome size\n\t-f for fasta\n\t-l a file with the lengths of the reads (can be created from this script with a fasta)\n\t-b size of the bins (in % of total length)\n/!\ if both -f and -l are given -l will have the priority\n\noutput:\n\t-o for output directory\n")
+	"""
+	Prints the usage and help 
+	"""
+	print("\npython plotReadLengths.py -f [sequences.fasta] -b [numberOfBins] -g [estimatedGenomeSize] -o [outputDirectory]")
+	print("")
+	print("Input:")
+	print("	-f: the input fasta file")
+	print("	-l: a length file, is created by this script. Can be used to run the script again more quickly.")
+	print('		/!\ If both -f and -l are specified, -l will have the priority.')
+	print("	-g: the estimated genome size, will be used to compute the coverage. Optional")
+	print("	-b: the number of bins on the plot. Default: 20")
+	print("Output:")
+	print("	-o: the output director. Default to " + outputDir)
+	print("Other:")
+	print("	-v: verbose mode.")
+	print("	-h: print the help and exit")
 
+
+
+#===================================
+#	 			Main
+#===================================
+# Getting the parameters from getopt
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "ho:f:l:g:b:", ["help", "output=", "fasta=", "lengthsFile=", "genomeEstimate=", "binSize="])
+	opts, args = getopt.getopt(sys.argv[1:], "ho:f:l:g:b:v", ["help", "output=", "fasta=", "lengthsFile=", "genomeEstimate=", "binNumber=", "verbose="])
 except getopt.GetoptError as err:
-	print str(err)  # will print "option -a not recognized"
+	print(str(err))  # will print "option -a not recognized"
 	usage()
 	sys.exit(2)
-    
 
 for o, a in opts:
 	if o in ("-f", "--fasta"):
-		fastq = a
+		fasta = a
 	elif o in ("-g", "--genomeSize"):
 		expectedGenomeSize = int(a)
-	elif o in ("-b", "--binSize"):
+	elif o in ("-b", "--binNumber"):
 		percent = int(a)
 	elif o in ("-o", "--output"):
 		outputDir = a
@@ -39,109 +63,115 @@ for o, a in opts:
 	elif o in ("-h", "--help"):
 		usage()
 		sys.exit()
+	elif o in ("-v", "--verbose"):
+		verbose = 1
 	else:
 		assert False, "unhandled option"
 
 
-if(fastq == None and lengthsFile == None):
+if(fasta == None and lengthsFile == None):
+	print("Please provide either a fasta file as input (option -f / --fasta) or a lengthsFile (option -l / --lengthsFile)\n")
 	usage()
-   	print("Please provide either a fastq file as input (option -f / --fastq) or a lengthsFile (option -l / --lengthsFile)")
-   	sys.exit()
+	sys.exit()
 
-if(fastq != None and not(os.path.isfile(fastq))):
-	print(fastq + " is not detected as a file... abort")
+if(fasta != None and not(os.path.isfile(fasta))):
+	print(fasta + " is not detected as a file. (option -f / --fasta)")
 	sys.exit()
 
 if(lengthsFile != None and not(os.path.isfile(lengthsFile))):
-	print(lengthsFile + " is not detected as a file... abort")
+	print(lengthsFile + " is not detected as a file. (option -l / --lengthsFile)")
 	sys.exit()
 
-if(outputDir == None):
-	usage()
-	print("Please provide output directory (option -o / --output)")
-	sys.exit()
-
-if(expectedGenomeSize == None):
-	usage()
-	print("Please provide expected Genome Size (option -g / --genomeSize)")
+if(not(os.path.isdir(outputDir))):
+	print(outputDir + " is not a folder. (option -o / --output)")
 	sys.exit()
 
 if(percent == None):
 	percent = 20
-	print("defaulting bin size to " + str(percent) + "% of Longest Read")
+	if(verbose == 1):
+		print("Defaulting bin size to " + str(percent) + "% of Longest Read.\n")
 
 
-# If there is no file with lengths available, we use the fastq (slower)
+# If there is no file with lengths available, we use the fasta (slower)
 if lengthsFile == None:
 	# Command to get reads length :
-	# awk '/^>/ {if (seqlen){print seqlen};seqlen=0;next; } { seqlen += length($0)}END{print seqlen}' pacbio.fasta
-	cmd = ["awk", '/^>/ {if (seqlen){print seqlen};seqlen=0;next; } { seqlen += length($0)}END{print seqlen}', fastq]
-	print cmd
-	lengths = Popen(cmd, stdout=PIPE).communicate()[0]
-	
-	# Create lengths file
-	filename = os.path.basename(fastq) + "_lengthsFile.txt"
-	print("Writing lengths to: " + filename + "...")
-	file = open(filename, "w")
-	file.write(lengths)
-	file.close()
-	print("Done")
+	cmd = ["awk", '/^>/ {if (seqlen){print seqlen};seqlen=0;next; } { seqlen += length($0)}END{print seqlen}', fasta]
+	if(verbose == 1):
+		print("Calculating the sequences length for the fasta...")
+		print(" ".join(cmd))
+	awk_process = Popen(cmd, shell = False, stdout=PIPE)
+	lengths, stderr = awk_process.communicate()
+	# Popen is outputing bytes, so we use "decode()" to change it to string
+	lengths_decoded = lengths.decode()
 
-	outputName = os.path.basename(fastq)
+	# Create the lengths file for potential future runs
+	filename = os.path.basename(fasta) + "_lengthsFile.txt"
+	if(verbose == 1):
+		print("Writing lengths to: " + filename + "...")
+	with open(filename, "w") as len_file_writer:
+		len_file_writer.write(lengths_decoded)
+		if(verbose == 1):
+			print("Done\n")
 
-# the "with" command ensure that the file is closed, even when an error occurs
+	outputName = os.path.basename(fasta)
+
+# If no fasta then we use the lengths file
 else:
-	with open(lengthsFile, 'r') as content_file:
-    		lengths = content_file.read()
+	with open(lengthsFile, 'r') as length_file_reader:
+    		lengths_decoded = length_file_reader.read()
 
 	outputName = os.path.basename(lengthsFile)
 
 
-# Read the lengths
-readLengths = lengths.split("\n")
-lengths = ""
+readLengths = lengths_decoded.split("\n")
 # Filter the empty ones if there are any
 readLengths = filter(None, readLengths)
 # Transform the lengths from string to int
-readLengths = map(int, readLengths)
+# in python3 map return a generator, so we convert to "list" to avoid issues with matplotlib
+readLengths = list(map(int, readLengths))
 
-print("Longest Read = " + str(max(readLengths)))
+maxRead = max(readLengths)
+if(verbose == 1):
+	print("Longest Read = " + str(maxRead))
 
 #  Get coverage as a string to print in the graph's title
-cov = int(sum(readLengths) / expectedGenomeSize)
-print("Coverage " + str(cov) + " X")
+if(expectedGenomeSize != None):
+	cov = int(sum(readLengths) / expectedGenomeSize)
+	if(verbose == 1):
+		print("Coverage " + str(cov) + " X")
 
 
 # Bin size is percent% of Longest Read:
 # Allows for the same bin size for each distributions
 p = percent / (100 * 1.0)
 print("percent : " + str(p))
-if(p*max(readLengths) == 0):
+if(p*maxRead == 0):
 	binSize = 1000
 else:
-	binSize = p * max(readLengths)
+	binSize = int(p * maxRead)
 
 # Setting the bin size
 print("Bin size = " + str(binSize))
-bins = np.arange(0, max(readLengths)+1, binSize)
+bins = np.arange(0, maxRead+1, binSize)
 
 # Plotting options 
 plt.figure(figsize=(15,8), facecolor='white')
 
-plt.xlim([0, max(readLengths)+1])
+plt.xlim([0, maxRead+1])
 #plt.xticks(bins)
 plt.xlabel('Read Length')
 plt.ylabel('Number of Reads')
 
 plt.hist(readLengths, bins=bins, alpha=0.5, facecolor='green')
 
-plt.suptitle("Read Lengths Distribution (Coverage " + str(cov) + " X)", fontsize = 16)
+if(expectedGenomeSize != None):
+	plt.suptitle("Read Lengths Distribution (Coverage " + str(cov) + " X)", fontsize = 16)
 plt.title(outputName, fontsize = 12)
 
 # Saving file to pdf
 GraphFile = outputDir + "/" + outputName + "_readLengths.pdf"
-print("Saving graph into " + GraphFile + "...")
 plt.savefig(GraphFile, bbox_inches='tight')
-print("Done")
+
+print("Graph saved to " + GraphFile)
 
 plt.show()
